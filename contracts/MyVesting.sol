43307ba2436f3f7405ecf0ffc84a11c9051f4b7e
Ismael Peña <ismael.ruben.pr@gmail.com>
2018-03-05 19:39:57 -0600

    update zepelin dependency version

--- contracts/MyVesting.sol
+++ contracts/MyVesting.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.19;
 
-import 'zeppelin-solidity/contracts/token/TokenVesting.sol';
+import 'zeppelin-solidity/contracts/token/ERC20/TokenVesting.sol';
 import 'zeppelin-solidity/contracts/examples/SimpleToken.sol';
-import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
 contract MyVesting{
 	
